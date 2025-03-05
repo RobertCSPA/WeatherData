fa79fc489d03c5ea5e4bb828994add7b654349b9
RobertCSPA <robertf127@nycstudents.net>
2025-03-05 17:06:04 +0000

    classes

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
-
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args){
+        double[] temps = {99.1, 142, 85, 86.1, 84.6, 94.3, 124.9, 98, 101, 102.5};
+        ArrayList<Double> t = new Arraylist<Double>();
+        for (Double n: temps){
+            t.add(n);
+        }
+        WeatherData = new WeatherData(t);
+    }
+}
--- WeatherData.java
+++ WeatherData.java
@@ -1,1 +1,3 @@
-
+public WeatherData(ArrayList<Double> temps){
+    temperatures = temps;
+}
