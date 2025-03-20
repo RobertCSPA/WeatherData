dcbed0e447f16c93fde448140a6622705efde55a
RobertCSPA <robertf127@nycstudents.net>
2025-03-20 16:32:22 +0000

    heatwave

--- WeatherData.java
+++ WeatherData.java
@@ -10,8 +10,22 @@ public class WeatherData{
     public void cleanData(Double lower, Double upper){
         for(int i = 0; i<temperatures.size(); i++){
             double temp = temperatures.get(i);
-            if(temp < lower || temp > upper)temperatures.remove(i);
+            if(temp < lower || temp > upper){
+                temperatures.remove(i);
+                i--;
+            }
         }
+    }
+
+    public int longestHeatWave(double threshold){
+        int heatWave = 0;
+        int max = 0;
+        for (double t: temperatures){
+            if (t > threshold)heatWave++;
+            if (heatWave > max) max = heatWave;
+            else heatWave = 0;
+        }
+        return max;
     }
 
     public String toString(){
