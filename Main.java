d48fd7f361ea2affadaca4f8b5eb579bbbe5da42
RobertCSPA <robertf127@nycstudents.net>
2025-03-20 16:19:53 +0000

    methods

--- Main.java
+++ Main.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 public class Main{
     public static void main(String[] args){
         double[] temps = {99.1, 142, 85, 86.1, 84.6, 94.3, 124.9, 98, 101, 102.5};
-        ArrayList<Double> t = new Arraylist<Double>();
+        ArrayList<Double> t = new ArrayList<Double>();
         for (Double n: temps){
             t.add(n);
         }
-        WeatherData = new WeatherData(t);
+        WeatherData w = new WeatherData(t);
+        w.cleanData(85.0, 120.0);
     }
 }
--- WeatherData.java
+++ WeatherData.java
@@ -1,3 +1,21 @@
-public WeatherData(ArrayList<Double> temps){
-    temperatures = temps;
+import java.util.ArrayList;
+
+public class WeatherData{
+    private ArrayList<Double> temperatures;
+
+    public WeatherData(ArrayList<Double> temps){
+        temperatures = temps;
+    }
+
+    public void cleanData(Double lower, Double upper){
+        for(int i = 0; i<temperatures.size(); i++){
+            double temp = temperatures.get(i);
+            if(temp < lower || temp > upper)temperatures.remove(i);
+        }
+    }
+
+    public String toString(){
+        return temperatures.toString();
+    }
+
 }
